8b4dbfb9e42b8dc6a50231b518674940b891698c
Eric <eyee01@students.cs.ubc.ca>
2023-02-03 16:48:07 -0800

    created Team stubs

--- src/main/model/Game.java
+++ src/main/model/Game.java
@@ -2,5 +2,8 @@ package model;
 
 // Represents a game played by a team that outlines
 // the other team that played, date, and result
+
+// IGNORE FOR NOW AND MAYBE DELETE IN THE FUTURE
+
 public class Game {
 }
--- src/main/model/Team.java
+++ src/main/model/Team.java
@@ -1,6 +1,43 @@
 package model;
 
-// represents a team with a name, players,
-// and games played
+import java.util.List;
+
+// represents a team with a name and players,
 public class Team {
+
+    private String name;
+    private List<Player> players;
+
+    // REQUIRES: teamName has a non-zero length
+    // EFFECTS: name is set to teamName;
+    //          players is set to an empty list of Player
+    public Team(String teamName) {
+        // stub
+    }
+
+    // REQUIRES: newTeamName has a non-zero length
+    // MODIFIES: this
+    // EFFECTS: name is set to newTeamName
+    public void changeTeamName(String newTeamName) {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: adds player to players
+    public void addPlayer(Player player) {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: removes player from players
+    public void removePlayer(Player player) {
+        // stub
+    }
+
+    // EFFECTS: lists all players' names
+    public String viewAllPlayers() {
+        return ""; // stub
+    }
+
+
 }
