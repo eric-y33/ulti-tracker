3313ed003df935a495c6df1c8a70ecf26d96d679
Eric <eyee01@students.cs.ubc.ca>
2023-03-22 18:35:41 -0700

    arranged all planned components

--- src/main/ui/GUI.java
+++ src/main/ui/GUI.java
@@ -8,13 +8,16 @@ import java.awt.*;
 
 public class GUI {
 
-    private JFrame mainFrame = new JFrame();
+    private JFrame mainFrame = new JFrame("Ultimate Team Manager");
     private JPanel rootPanel = new JPanel();
 
+    private JPanel saveOptions = new JPanel();
     private JPanel playerPanel = new JPanel();
     private JPanel playerOptions = new JPanel();
-    private JPanel saveOptions = new JPanel();
 
+
+    private JButton save = new JButton("Save");
+    private JButton load = new JButton("Load");
 
     private JList playerList = new JList();
     private JTextField playerInfo = new JTextField("Test");
@@ -28,23 +31,53 @@ public class GUI {
         mainFrame.setResizable(false);
         mainFrame.setMinimumSize(new java.awt.Dimension(500, 300));
 
+        rootPanel.setLayout(new BorderLayout());
+
+        setupSaveOptions();
         setupPlayerPanel();
+        setupPlayerOptions();
 
-
+        rootPanel.add(saveOptions, BorderLayout.PAGE_START);
         rootPanel.add(playerPanel);
-        rootPanel.add(playerOptions);
+        rootPanel.add(playerOptions, BorderLayout.PAGE_END);
 
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainFrame.pack();
         mainFrame.setVisible(true);
     }
 
+    private void setupSaveOptions() {
+        saveOptions.setLayout(new FlowLayout());
+        saveOptions.add(save);
+        saveOptions.add(load);
+    }
+
     private void setupPlayerPanel() {
-        // TODO
+
+        playerPanel.setBackground(Color.BLUE);
+
+        String data[]= { "Monday","Tuesday","Wednesday",
+                "Thursday","Friday","Saturday"};
+
+        playerList = new JList(data);
+        playerList.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+        playerList.setLayoutOrientation(JList.VERTICAL);
+        playerList.setVisibleRowCount(-1);
+
+        JScrollPane listScroller = new JScrollPane(playerList);
+        listScroller.setPreferredSize(new Dimension(250, 200));
+
+        playerInfo.setPreferredSize(new Dimension(250, 200));
+
+        playerInfo.setEditable(false);
+        playerPanel.add(listScroller, BorderLayout.WEST);
+        playerPanel.add(playerInfo, BorderLayout.EAST);
     }
 
     private void setupPlayerOptions() {
-        // TODO
+        playerOptions.setLayout(new FlowLayout());
+        playerOptions.add(addPlayer);
+        playerOptions.add(removePlayer);
     }
 
     public static void main(String[] args) {
