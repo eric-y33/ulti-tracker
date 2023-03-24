1a2d8471ef8825e836665f0143cc76c1d3d932a1
Eric <eyee01@students.cs.ubc.ca>
2023-03-23 17:04:20 -0700

    added comments and made team load in from json

--- src/main/ui/GUI.java
+++ src/main/ui/GUI.java
@@ -1,10 +1,22 @@
 package ui;
 
+import model.Team;
+import persistence.JsonReader;
+import persistence.JsonWriter;
+
 import javax.swing.*;
 
 import java.awt.*;
+import java.io.IOException;
+import java.util.List;
 
 public class GUI {
+
+    private Team team;
+    private static final String JSON_STORE = "./data/team.json";
+    private JsonWriter jsonWriter = new JsonWriter(JSON_STORE);;
+    private JsonReader jsonReader = new JsonReader(JSON_STORE);;
+
 
     private JFrame mainFrame = new JFrame("Ultimate Team Manager");
     private JPanel rootPanel = new JPanel();
@@ -25,6 +37,8 @@ public class GUI {
 
     private GUI() {
 
+        loadData(); // make this run after pressing the load data button
+
         mainFrame.setContentPane(rootPanel);
         mainFrame.setResizable(false);
         mainFrame.setMinimumSize(new java.awt.Dimension(500, 300));
@@ -44,12 +58,25 @@ public class GUI {
         mainFrame.setVisible(true);
     }
 
+    // MODIFIES: this
+    // EFFECTS: loads team from file
+    private void loadData() {
+        try {
+            team = jsonReader.read();
+            System.out.println("Loaded " + team.getTeamName() + " from " + JSON_STORE);
+        } catch (IOException e) {
+            System.out.println("Unable to read from file: " + JSON_STORE);
+        }
+    }
+
+    // EFFECTS: sets up save options panel
     private void setupSaveOptionsPanel() {
         saveOptions.setLayout(new FlowLayout());
         saveOptions.add(save);
         saveOptions.add(load);
     }
 
+    // EFFECTS: sets up main player panel
     private void setupPlayerPanel() {
 
         playerPanel.setBackground(Color.BLUE);
@@ -62,17 +89,26 @@ public class GUI {
         playerPanel.add(playerInfo, BorderLayout.EAST);
     }
 
+    // EFFECTS: adjusts player info component to be empty or have the selected player's info
     private void adjustPlayerInfoComponent() {
         playerInfo.setPreferredSize(new Dimension(250, 200));
         playerInfo.setEditable(false);
         playerInfo.append("Test");
     }
 
+    // EFFECTS: adjusts player list component to have the list of players
     private JScrollPane adjustPlayerListComponent() {
-        String data[]= { "Monday","Tuesday","Wednesday",
-                "Thursday","Friday","Saturday"};
+//        String data[]= { "Monday","Tuesday","Wednesday",
+//                "Thursday","Friday","Saturday"};
 
-        playerList = new JList(data);
+        List<String> teamListOfPlayerNames = team.getListOfPlayerNames();
+
+        DefaultListModel<String> playerModel = new DefaultListModel<>();
+        for (String name : teamListOfPlayerNames) {
+            playerModel.addElement(name);
+        }
+
+        playerList = new JList(playerModel);
         playerList.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
         playerList.setLayoutOrientation(JList.VERTICAL);
         playerList.setVisibleRowCount(-1);
@@ -82,12 +118,14 @@ public class GUI {
         return listScroller;
     }
 
+    // EFFECTS: sets up player options panel
     private void setupPlayerOptionsPanel() {
         playerOptions.setLayout(new FlowLayout());
         playerOptions.add(addPlayer);
         playerOptions.add(removePlayer);
     }
 
+    // EFFECTS: starts GUI application
     public static void main(String[] args) {
         new GUI();
     }
